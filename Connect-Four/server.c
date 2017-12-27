33c8de8c6fec3bd858472ac29d9dde4894ba426a
loghinalexandru <loghinalexandru61@gmail.com>
2017-12-27 17:32:47 +0200

    Add files via upload

--- Connect-Four/server.c
+++ Connect-Four/server.c
@@ -17,12 +17,16 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <pthread.h>
+#include <fcntl.h>
 
 /* portul folosit */
 #define PORT 2024
 #define MAX_GAME_HOSTED 40
 #define RED_PLAYER  2
 #define YELLOW_PLAYER  3
+#define MAX_ROW 7 
+#define MAX_COL 8
+#define PLAYER_WIN 5
 
 /* codul de eroare returnat de anumite apeluri */
 extern int errno;
@@ -31,10 +35,51 @@ typedef struct thData{
 	int idThread; //id-ul thread-ului tinut in evidenta de acest program
 	int player_one;
   int player_two; //descriptorul intors de accept
+  int first_player_color;
 }thData;
 
+
+
 static void *treat(void *); /* functia executata de fiecare thread ce realizeaza comunicarea cu clientii */
-void raspunde(void *);
+
+
+int check_win_condition(char playing_matrix[MAX_ROW][MAX_COL]) { 
+     for(int i = 0 ; i < MAX_ROW ; ++i){
+       for(int j = 0 ; j < MAX_COL; ++j){
+         printf("%i" , (int) playing_matrix[i][j]);
+       }
+       printf("\n");
+     }
+     for (int i = 1; i < MAX_ROW; ++i) {
+     for (int j = 1; j < MAX_COL; ++j) {
+     char sum_line = 1, sum_col = 1, sum_dig = 1, sum_dig_rev = 1;
+     if (playing_matrix[i][j] !=  0) {
+     for (int t = 1; t < 4; ++t) {
+     if (j + t < MAX_COL) {
+          if (playing_matrix[i][j + t] == playing_matrix[i][j])
+                sum_line++; 
+     }
+     if (i + t < MAX_ROW) {
+         if (playing_matrix[i + t][j] == playing_matrix[i][j])
+             sum_col++;
+     }
+     if (i + t < MAX_ROW && j - t > 0) {
+         if (playing_matrix[i + t][j - t] == playing_matrix[i][j])
+              sum_dig++;
+     }
+     if (i + t < MAX_ROW && j + t < MAX_ROW) {
+          if (playing_matrix[i + t][j + t] == playing_matrix[i][j])
+                          sum_dig_rev++;
+     }
+     }
+      if (sum_line >= 4 || sum_col >= 4 || sum_dig >= 4 || sum_dig_rev >= 4) 
+              return playing_matrix[i][j];
+     }
+     }
+      
+}
+        return 0;
+}
 
 int main ()
 {
@@ -42,13 +87,12 @@ int main ()
   struct sockaddr_in player_one;	
   struct sockaddr_in player_two;	
   int number_of_players = 0;
+  signal(SIGPIPE, SIG_IGN);
   int nr;		//mesajul primit de trimis la client 
   int sd;		//descriptorul de socket 
   int pid;
   pthread_t th[100];    //Identificatorii thread-urilor care se vor crea
 	int i=0;
-  
-
   /* crearea unui socket */
   if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1)
     {
@@ -91,7 +135,7 @@ int main ()
     {
       int fd_player_one;
       int fd_player_two;
-      char set_color = rand() % 45 + 1;
+      char set_color = rand() % 80;
       thData * td; //parametru functia executata de thread     
       int length_player_one = sizeof(player_one);
       int length_player_two = sizeof(player_two);
@@ -109,9 +153,9 @@ int main ()
 	  continue;
 	}
   else{
-    char first_or_second = set_color & 1;
+    char first_or_second = set_color % 2;
     write(fd_player_one , &first_or_second , 1);
-    first_or_second = set_color & 0;
+    first_or_second = (set_color + 1) % 2;
     write(fd_player_one , &first_or_second , 1);
       ++number_of_players;
   }
@@ -121,9 +165,9 @@ int main ()
 	  continue;
 	}
   else{
-   char first_or_second = set_color & 0;
+   char first_or_second = (set_color + 1) % 2;
     write(fd_player_two , &first_or_second , 1);
-    first_or_second = set_color & 1;
+    first_or_second = set_color % 2;
     write(fd_player_two , &first_or_second , 1);
       ++number_of_players;
   }
@@ -136,6 +180,7 @@ int main ()
 	td->idThread=i++;
 	td->player_one = fd_player_one;
   td->player_two = fd_player_two;
+  td->first_player_color = set_color % 2;
   if(number_of_players == 2){
     number_of_players = 0;
 	  pthread_create(&th[i], NULL, &treat, td);	   
@@ -144,60 +189,70 @@ int main ()
 };				
 static void *treat(void * arg)
 {		
-    char game_matrix[7][8];
-    int win_condition = 0, random_exit = 0;
+    char game_matrix[MAX_ROW][MAX_COL];
+    char win_condition = 0, random_exit = 0;
     int turn_number = 0;
+   
 		struct thData* game_info;
     memset(game_matrix , 0 , sizeof(game_matrix));
 		game_info = (struct thData*) arg;
-    while(!win_condition || !random_exit){
+    while(!win_condition  && !random_exit){
           char a[20];
-          printf("%i\n" , turn_number);
           fflush(stdout);
           char player_turn = turn_number % 2;
           memset(a , 0 , sizeof(a));
-          write(game_info->player_one , &player_turn , 1);
+          if(write(game_info->player_one , &player_turn , 1) == -1){
+            random_exit = 1;
+            if(game_info->first_player_color != 0){
+                win_condition = YELLOW_PLAYER;
+            }
+            else{
+                win_condition = RED_PLAYER;
+            }
+            break;
+          }
           player_turn = (turn_number + 1) % 2;
-          write(game_info->player_two , &player_turn , 1);
-
+          if(write(game_info->player_two , &player_turn , 1) == -1){
+            random_exit = 1;
+            if(game_info->first_player_color == 0){
+                win_condition = YELLOW_PLAYER;
+            }
+            else{
+                win_condition = RED_PLAYER;
+            }
+            break;
+          }
           if(turn_number % 2 == 1){
               read(game_info->player_one , a , sizeof(a));
               int i;
               int x = -1 , y =  -1;
               x = a[1] - 48;
               y = a[3] - 48;
-              printf("%i %i\n" ,x , y);
-              for(int i = x; i <= 5; ++i){
+              for(int i = x; i < 6; ++i){
                   if(game_matrix[i+1][y] == 0){
                     ++x;
                   }
                   else
                     break;
               }
-       
-              game_matrix[x][y] = RED_PLAYER;
-              int j;
-              for(int i = 1; i <= 6; ++i){
-                  for(int j = 1 ; j <= 7 ;++j){
-                    printf("%i" , (int) game_matrix[i][j]);
-                  }
-                  printf("\n");
+              if(game_info->first_player_color == 0){
+                  game_matrix[x][y] = YELLOW_PLAYER; 
+              }
+              else{
+                  game_matrix[x][y] = RED_PLAYER;
               } 
-              printf("%s\n" , a); 
               a[1] = x + 48;
-              printf("%s\n" , a);     
-              fflush(stdout);
+              win_condition = check_win_condition(game_matrix);
+              printf("\nWIN CONDITION : %i" , win_condition);  
               write(game_info->player_two , a , sizeof(a));
               write(game_info->player_one , a , sizeof(a));
-            
           }
           else{
-            read(game_info->player_two , a , sizeof(a));
-            int i;
-              int x = -1 , y =  -1;
+              read(game_info->player_two , a , sizeof(a));
+              int i;
+              int x = -1 , y = -1;
               x = a[1] - 48;
               y = a[3] - 48;
-              printf("%i %i\n" ,x , y);
               for(int i = x ; i < 6; ++i){
                   if(game_matrix[i + 1][y] == 0){
                     ++x;
@@ -206,27 +261,28 @@ static void *treat(void * arg)
                     break;
                   }
               }
-              game_matrix[x][y] = YELLOW_PLAYER;
-              int j;
-              for(int i = 1; i <= 6; ++i){
-                  for(int j = 1 ; j <= 7 ;++j){
-                    printf("%i" , (int) game_matrix[i][j]);
-                  }
-                  printf("\n");
-              }              
-              printf("%s\n" , a);
+              if(game_info->first_player_color != 0){
+                  game_matrix[x][y] = YELLOW_PLAYER; 
+              }
+              else{
+                  game_matrix[x][y] = RED_PLAYER;
+              }   
               a[1] = x + 48;
-              printf("%s\n" , a);     
-              fflush(stdout);
+              win_condition = check_win_condition(game_matrix);
+              printf("\nWIN CONDITION : %i" , win_condition);
+              write(game_info->player_two , a , sizeof(a)); 
               write(game_info->player_one , a , sizeof(a));
-              write(game_info->player_two , a , sizeof(a));              
+         
                    
           }
           ++turn_number;
     }
+    printf("\n");
+    printf("%i" ,(int)write(game_info->player_two , &win_condition , sizeof(win_condition)));
+    printf("%i" , (int)write(game_info->player_one , &win_condition , sizeof(win_condition)));
+    fflush(stdout);
     close(game_info->player_one);
     close(game_info->player_two);
 		return(NULL);	
   		
 };
-
